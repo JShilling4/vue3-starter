5e517b298f0e26516112a38eb111e474e0b5631c
Justin Shilling <loveofmusic25@hotmail.com>
2021-07-10 10:04:35 -0400

    package update

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    configureWebpack: (config) => {
-        config.devtool = "source-map";
-    },
-    css: {
-        loaderOptions: {
-            sass: {
-                prependData: `
+	configureWebpack: (config) => {
+		config.devtool = "source-map";
+	},
+	css: {
+		loaderOptions: {
+			sass: {
+				prependData: `
                     @import "@/assets/scss/_variables.scss";
                     @import "@/assets/scss/_mixins.scss";
                  `,
-                sourceMap: true,
-            },
-        },
-        sourceMap: true,
-    },
+				sourceMap: true,
+			},
+		},
+		sourceMap: true,
+	},
 
-    devServer: {
-        host: "localhost",
-    },
+	devServer: {
+		host: "localhost",
+	},
 };
